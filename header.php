979557d36bb2febcc7720042a1215476cb5f1efb
KingKrazed <keithlee1108@gmail.com>
2021-09-29 22:46:03 +0100

    1

---
+++ header.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ */
+
+?>
+<?php
+$metaGeneratorContent = 'Nicepage 3.26.0, nicepage.com';
+$meta_generator = '';
+if ($metaGeneratorContent) {
+    remove_action('wp_head', 'wp_generator');
+    $meta_generator = '<meta name="generator" content="' . $metaGeneratorContent . '" />' . "\n";
+    $GLOBALS['meta_generator'] = true;
+}
+$hideHeader = false; // default header is visible
+global $hideFooter;
+$hideFooter = false; // default footer is visible
+$pageBlog = is_home();
+$pagePost = is_single();
+$pageProducts = theme_woocommerce_enabled() ? is_shop() || is_product_category() : false;
+$pageProduct = theme_woocommerce_enabled() ? is_product() : false;
+$pageCart = theme_woocommerce_enabled() ? is_cart() : false;
+$defaultPath = $pageProducts || $pageProduct || $pageCart ? '/woocommerce' : '';
+if ($pageBlog) {
+    $template = 'blog';
+}
+if ($pagePost) {
+    $template = 'post';
+}
+if ($pageProducts) {
+    $template = 'products';
+}
+if ($pageProduct) {
+    $template = 'product';
+}
+if ($pageCart) {
+    $template = 'cart';
+}
+$wpCustomTemplate = false;
+global $isWpCustomTemplate, $blog_custom_template, $post_custom_template;
+if ($isWpCustomTemplate) {
+    $template = $blog_custom_template ? $blog_custom_template : $post_custom_template;
+    if ($template) {
+        $wpCustomTemplate = true;
+    }
+}
+if ($pageBlog || $pagePost || $pageProducts || $pageProduct || $pageCart || $wpCustomTemplate) {
+    $defaultName = $pageCart ? '‌shoppingCart' : $template;
+    global ${$template . "_custom_template"};
+    ${$template . "_custom_template"} = ${$template . "_custom_template"} ? ${$template . "_custom_template"} : $defaultName . 'Template';
+    $customPath = $wpCustomTemplate ? $template : ${$template . "_custom_template"};
+    $fileWithOptions = get_template_directory() . $defaultPath . '/template-parts/' . $customPath . '/custom-template-options.php';
+    if ( file_exists( $fileWithOptions ) ) {
+        include_once $fileWithOptions;
+    }
+} ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js" style="font-size:<?php echo apply_filters('theme_base_font_size', '16'); ?>px">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php echo $meta_generator; ?>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php endif; ?>
+    <?php wp_head(); ?>
+    
+    
+    
+</head>
+
+<body <?php body_class(); ?><?php body_style_attribute(); ?> <?php body_data_attributes(); ?>>
+<?php if (version_compare( $wp_version, '5.2', '>=' )) { wp_body_open(); } ?>
+<div id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'genos' ); ?></a>
+    <?php if (!$hideHeader) { ?>
+    <header class="u-clearfix u-header u-header" id="sec-b369">
+  <div class="u-clearfix u-sheet u-sheet-1">
+    <?php $logo = theme_get_logo(array(
+            'default_src' => "/images/bd8a07a77e1a960c6970357de60fad1c.jpg",
+            'default_url' => "https://nicepage.com"
+        )); $url = stripos($logo['url'], 'http') === 0 ? esc_url($logo['url']) : $logo['url']; ?><a <?php if (is_customize_preview()) echo 'data-default-src="' . esc_url($logo['default_src']) . '" '; ?>href="<?php echo $url; ?>" class="u-image u-logo u-image-1 custom-logo-link" data-image-width="550" data-image-height="589">
+      <img <?php if ($logo['svg']) { echo 'style="width:'.$logo['width'].'px"'; } ?>src="<?php echo esc_url($logo['src']); ?>" class="u-logo-image u-logo-image-1">
+    </a>
+    <img class="u-image u-image-default u-image-2" src="<?php
+                                $header_image = get_header_image();
+                                if ($header_image) {
+                                    echo esc_url($header_image);
+                                } else {
+                                    ?><?php echo get_template_directory_uri(); ?>/images/c47cc0052a849ba84f5f5d4cabd9bb11.png<?php
+                                }
+                            ?>" alt="" data-image-width="362" data-image-height="64">
+  </div>
+</header>
+    
+    <?php } ?>
+    <div id="content">
---
+++ template-parts/footer-styles.php
@@ -0,0 +1,47 @@
+<!-- footer styles -->
+
+<style>.u-footer .u-sheet-1 {
+  min-height: 120px;
+}
+.u-footer .u-social-icons-1 {
+  height: 32px;
+  min-height: 16px;
+  width: 158px;
+  min-width: 94px;
+  white-space: nowrap;
+  margin: 44px auto;
+}
+.u-footer .u-icon-1 {
+  color: rgb(3, 155, 229) !important;
+}
+.u-footer .u-icon-2 {
+  color: rgb(85, 172, 238) !important;
+}
+.u-footer .u-icon-3 {
+  color: rgb(197, 54, 164) !important;
+  height: 32px;
+}
+.u-footer .u-icon-4 {
+  color: rgb(0, 122, 185) !important;
+  height: 32px;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-sheet-1 {
+    min-height: 99px;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-sheet-1 {
+    min-height: 76px;
+  }
+}
+@media (max-width: 767px) {
+  .u-footer .u-sheet-1 {
+    min-height: 57px;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-sheet-1 {
+    min-height: 36px;
+  }
+}</style>
